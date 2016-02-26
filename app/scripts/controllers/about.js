75ed408a3358d6dacab79d9848cc01a15ab286b3
Samuel Ortiz Reina <iamsortiz@gmail.com>
2016-02-26 14:23:00 +0100

    Yeoman angular generator created

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swipe4goodApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the swipe4goodApp
+ */
+angular.module('swipe4goodApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swipe4goodApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the swipe4goodApp
+ */
+angular.module('swipe4goodApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
