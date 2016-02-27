b00536e68cc4bfdf99f7e1a43f38cf4d1be1c3c0
Samuel Ortiz Reina <iamsortiz@gmail.com>
2016-02-27 02:53:54 +0100

    [DONE] splash screen

---
+++ app/scripts/controllers/splash.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swipe4goodApp.controller:SplashCtrl
+ * @description
+ * # SplashCtrl
+ * Controller of the swipe4goodApp
+ */
+angular.module('swipe4goodApp')
+    .controller('SplashCtrl', ['$scope', function($scope) {
+    }]);
